dcee8a9fb2a14ca5523e4e569e2bb32ec2d0133a
Sammy Soudan <mr.sammysoudan@gmail.com>
2024-11-24 03:32:39 +0000

    portfolio web

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+const Footer = () => (
+  <footer className='footer'>
+    <a
+      href='https://github.com/mrsammysoudan/sammysoudan.github.io'
+      className='link footer__link'
+    >
+      Sammy Soudan
+    </a>
+  </footer>
+)
+
+export default Footer
---
+++ src/portfolio.js
@@ -0,0 +1,88 @@
+const header = {
+  // all the properties are optional - can be left empty or deleted
+  homepage: 'https://rjshkhr.github.io/cleanfolio',
+  title: 'SS.',
+}
+
+const about = {
+  // all the properties are optional - can be left empty or deleted
+  name: 'Sammy Soudan',
+  role: 'Software and App Developer',
+  description:
+    'Professional Developer based in London, UK with industrial experience. I enjoy challenging tasks and always aim to achieve creativity and logic',
+  resume: 'https://example.com',
+  social: {
+    linkedin: 'https://www.linkedin.com/in/sammy-soudan/',
+    github: 'https://github.com',
+  },
+}
+
+const projects = [
+  // projects can be added an removed
+  // if there are no projects, Projects section won't show up
+  {
+    name: 'Dishify',
+    description:
+      'Founded Dishify, an app that uses AI and image recognition to create recipes based on ingredients',
+    stack: [
+      'Flutter',
+      'Laravell',
+      'Python',
+      'TensorFlow',
+      'Jetpack Compose',
+      'Kotlin',
+    ],
+    sourceCode: 'https://github.com/mrsammysoudan/Dishify/tree/main',
+    livePreview: 'https://dishify.co.uk',
+  },
+  {
+    name: 'Fitness Tracker',
+    description:
+      'Developed a Web application that tracks and suggests fitness excersies based on your body capabilities',
+    stack: [
+      'Node',
+      'TypeScript',
+      'React',
+      'PostgreSQL',
+      'AWS',
+      'GraphQL',
+      'RESTful',
+    ],
+    sourceCode: 'https://github.com/mrsammysoudan/fitnessDam',
+  },
+  {
+    name: 'Prayer Coach',
+    description:
+      'Developed a prayer app with leveling up system to track the prayers using voice recognition',
+    stack: ['Flutter', 'Figma', 'RESTful'],
+    sourceCode: 'https://github.com/mrsammysoudan/azkar_coach',
+  },
+]
+
+const skills = [
+  // skills can be added or removed
+  // if there are no skills, Skills section won't show up
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'TypeScript',
+  'React',
+  'Flutter',
+  'Laravell',
+  'Figma',
+  'Git',
+  'Node',
+  'AWS',
+  'PostgreSQL',
+  'GraphQL',
+  'Python',
+  'Kotlin',
+  'Java',
+]
+
+const contact = {
+  // email is optional - if left empty Contact section won't show up
+  email: 'mr.sammysoudan@gmail.com',
+}
+
+export { header, about, projects, skills, contact }
